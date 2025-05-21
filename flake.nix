9dbd0960d3c13f3afe54fa15a0c9959affb3e85a
0xPhase <dev@phase.cash>
2025-05-21 19:37:56 +0300

    Updates to SDK

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      eachSystem =
+        f:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = eachSystem (pkgs: {
+        default = with pkgs; mkShell {
+          packages = [
+            nodejs
+
+            corepack
+            bun
+
+            nodePackages.typescript
+            nodePackages.typescript-language-server
+            brave
+          ];
+
+          shellHook = ''
+            export BRAVE_BROWSER=${pkgs.brave}
+          '';
+        };
+      });
+    };
+}
