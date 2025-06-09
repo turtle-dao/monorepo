ac183232e631367c23364f33edb5b881b526f4ad
Mikhail Gladchenko <m@gladchenko.pw>
2025-06-09 19:05:08 +0100

    Widget feature (#1)
    * Add initial setup for widget module:
    
    - Include Tailwind, utility functions, and components.
    - Define constants for tabs and export utilities.
    - Add base HTML file for the widget.
    
    * Update package.json (to trigger new deployment)
    
    * Bun update
    
    * Update widget HTML title to "Turtle Club Protocol"
    
    ---------
    
    Co-authored-by: 0xFelix <116054565+0xPhase@users.noreply.github.com>
    Co-authored-by: 0xPhase <dev@phase.cash>

---
+++ src/widget/src/constants/tabs.ts
@@ -0,0 +1,9 @@
+export const TAB_TURTLE_EARN = "earn";
+export const TAB_YOUR_POSITIONS = "positions";
+
+export const tabButtons = [
+  { label: "Turtle Earn", value: TAB_TURTLE_EARN },
+  { label: "Your Positions", value: TAB_YOUR_POSITIONS },
+] as const;
+
+export type TabType = (typeof tabButtons)[number]["value"];
