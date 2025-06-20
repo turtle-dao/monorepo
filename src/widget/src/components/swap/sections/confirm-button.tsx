3b120dabc00d331d43b5c02ac00b22d8a3c505e4
amilcarrey <amilcarrey.ar@gmail.com>
2025-06-20 04:33:16 -0300

    feat: implement transaction confirmation logic in ConfirmButton component
    - Added handleConfirm function to process transaction confirmations using the Wagmi adapter.
    - Updated button rendering to dynamically create confirmation buttons based on fetched route steps. This is temporary solution.

--- src/widget/src/components/swap/sections/confirm-button.tsx
+++ src/widget/src/components/swap/sections/confirm-button.tsx
@@ -1,4 +1,7 @@
+import type { Network } from "@turtledev/react/types/components/exported/earn-page/adapters/type";
+import type { Address, Hex } from "viem";
 import { useConnectModal } from "@rainbow-me/rainbowkit";
+import { useWagmiAdapter } from "@turtledev/react";
 import { type JSX, useEffect } from "react";
 import { useAccount } from "wagmi";
 import { TurtleButton } from "@/components/ui/TurtleButton";
@@ -9,6 +12,7 @@ export function ConfirmButton({ className }: { className?: string }): JSX.Elemen
 
   const { openConnectModal } = useConnectModal();
   const { fetchedRoute, routeError } = useTokenDeposit();
+  const { sendTransaction } = useWagmiAdapter();
 
   useEffect(() => {
     console.warn("fetchedRoute", fetchedRoute);
@@ -23,6 +27,27 @@ export function ConfirmButton({ className }: { className?: string }): JSX.Elemen
   // TODO: Handle Swap
   // TODO: Handle Click Action and Button message based on txStep
 
+  const handleConfirm = async (tx: {
+    data: string;
+    value: string;
+    from: string;
+    to: string;
+    gas: number;
+  }): Promise<void> => {
+    if (!fetchedRoute)
+      return;
+
+    // const tx = fetchedRoute.steps[1].tx;
+    const hash = await sendTransaction({
+      from: tx.from as Address,
+      to: tx.to as Address,
+      data: tx.data as Hex,
+      value: BigInt(tx.value),
+      chainId: 1 as Network,
+    });
+    console.warn("hash", hash);
+  };
+
   return address === undefined
     ? (
         <TurtleButton variant="reversed" size="lg" fullWidth onClick={openConnectModal} className={className}>
@@ -30,16 +55,23 @@ export function ConfirmButton({ className }: { className?: string }): JSX.Elemen
         </TurtleButton>
       )
     : (
-        <TurtleButton
-          variant="reversed"
-          size="lg"
-          fullWidth
-          // disabled={txStep !== TxStepValues.DONE && (confirmButtonDisabled || isConfirming || isPending)}
-          // eslint-disable-next-line no-alert
-          onClick={() => { alert("Confirm Transaction"); }}
-          className={className}
-        >
-          Confirm
-        </TurtleButton>
+        <>
+          {fetchedRoute && (
+            fetchedRoute.steps.map((step, index) => (
+              <TurtleButton
+                key={index}
+                variant="reversed"
+                size="lg"
+                fullWidth
+                // disabled={routeError !== undefined || fetchedRoute === undefined || fetchedRoute?.steps.length === 0}
+                onClick={() => handleConfirm(step.tx)}
+                className={className}
+              >
+                Confirm
+              </TurtleButton>
+            ))
+          )}
+        </>
+
       );
 }
